909229831d5f46e674bf316480d7bff516869160
Betelhem-mo <betelmolaw@gmail.com>
2026-04-14 19:20:42 +0300

    demo link added

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,10 +1,15 @@
 import homeProfile from "./homePageProfilePicture.png";
 import aboutProfile from "./aboutPageProfilePicture.png";
-
+import dormMaintenanceReporter from "./dorm-maintenance-reporter.png";
+import classScheduleReminder from "./class-schedule-reminder.png";
+import senbetBete from "./senbet-bete.png"
 
 
 
 export {
     homeProfile,
-    aboutProfile
+    aboutProfile,
+    dormMaintenanceReporter,
+    classScheduleReminder,
+    senbetBete
 };
